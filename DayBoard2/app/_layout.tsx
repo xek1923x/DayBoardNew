9d4de672cf41a5369a79603f26e0369c54c9547b
LordRoboter <lordroboter@outlook.de>
2024-12-11 08:36:41 +0000

    Co-authored-by: Eimen Kiose <xek1923x@users.noreply.github.com> Co-authored-by: Jaki27 <Jaki27grb@users.noreply.github.com>

--- DayBoard2/app/_layout.tsx
+++ DayBoard2/app/_layout.tsx
@@ -2,5 +2,5 @@ import { Stack } from "expo-router";
 import { View, Text} from "react-native";
 
 export default function RootLayout() {
-   return (Stack);
+   return <Stack/>;
 }
--- DayBoard2/app/index.tsx
+++ DayBoard2/app/index.tsx
@@ -1,16 +1,41 @@
-import React from "react";
-import { Text, View, StyleSheet, Platform } from "react-native";
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import { Calendar, Agenda } from 'react-native-calendars';
 
-export default function Index() {
+
+const MyCalendar = () => {
+
+  const [items, setItems] = useState({
+    '2024-04-29': [{ name: 'Deutsch Arbeit', time: '10:00 AM' }],
+    '2024-04-30': [{ name: 'Englisch Arbeit', time: '9:00 AM' }, { name: 'Project presentation', time: '2:00 PM' }, { name: 'Project presentation', time: '5:00 PM' }],
+    '2024-05-01': [{ name: 'China-Austausch', time: '9:00 AM' }, { name: 'Project presentation', time: '2:00 PM' }],
+    '2024-12-11': [{ name: 'Digitechnikum', time: '8:00 AM' }, { name: 'Big Band', time: '3:00 PM' }],
+  });
+
+
+  const [selectedDate, setSelectedDate] = useState(); 
+  
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1 }}>
+      <Calendar
+        markedDates={{
+          [selectedDate]: { selected: true, marked: true, selectedColor: 'blue' }, 
+        }}
+        onDayPress={(day) => { setSelectedDate(day.dateString); }} 
+      />
+
+    <Agenda
+        items={items} 
+        renderItem={(item) => (
+          <View style={{ marginVertical: 10, marginTop: 30,backgroundColor:'white',marginHorizontal:10,  padding: 10 }}>
+           <Text style={{ fontWeight: 'bold' }}>{item.name}</Text>
+            <Text>{item.time}</Text>
+        </View>
+        )} 
+      />
+
     </View>
   );
 }
+
+export default MyCalendar;
