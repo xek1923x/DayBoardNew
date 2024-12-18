8e3d41f568173742cce0bd67b5681005581d91cf
LordRoboter <lordroboter@outlook.de>
2024-12-18 08:35:26 +0000

    sufoaufiza

--- DayBoard2/app/index.tsx
+++ DayBoard2/app/index.tsx
@@ -1,44 +1,60 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
-import { Calendar, Agenda, CalendarList } from 'react-native-calendars';
-import { LocaleConfig } from 'react-native-calendars';
+import { View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 
-
-const undefineder = {key: 'Arbeit', color: 'red', selectedDotColor: 'blue'};
-const veranstaltung = {key: 'Arbeit', color: 'red', selectedDotColor: 'blue'};
-
+class CalendarItem
+// Ich hab das Volumen einer Kugel dabei 4 drittel pi mal r hoch 3
 const MyCalendar = () => {
   const [items, setItems] = useState({
-    '2024-04-29': [{dots: undefineder, name: 'Deutsch Arbeit', time: '10:00 AM' }],
-    '2024-04-30': [{ name: 'Englisch Arbeit', time: '9:00 AM' }, { name: 'Project presentation', time: '2:00 PM' }, { name: 'Project presentation', time: '5:00 PM' }],
-    '2024-05-01': [{ name: 'China-Austausch', time: '9:00 AM' }, { name: 'Project presentation', time: '2:00 PM' }],
-    '2024-12-11': [{ name: 'Digitechnikum', time: '8:00 AM' }, { name: 'Big Band', time: '3:00 PM' }],
+    '2024-04-29': [
+      { dots: [{ key: 'work', color: 'red', selectedDotColor: 'blue' }], name: 'Deutsch Arbeit', time: '10:00 AM' },
+    ],
+    '2024-04-30': [
+      { name: 'Englisch Arbeit', time: '9:00 AM' },
+      { name: 'Project presentation', time: '2:00 PM' },
+      { name: 'Project presentation', time: '5:00 PM' },
+    ],
+    '2024-05-01': [
+      { name: 'China-Austausch', time: '9:00 AM' },
+      { name: 'Project presentation', time: '2:00 PM' },
+    ],
+    '2024-12-11': [
+      { name: 'Digitechnikum', time: '8:00 AM' },
+      { name: 'Big Band', time: '3:00 PM' },
+    ],
   });
 
+  const [selectedDate, setSelectedDate] = useState();
 
-  const [selectedDate, setSelectedDate] = useState(); 
-  
+  const markedDates = Object.keys(items).reduce((termine, date) => {
+    termine[date] = {
+      marked: true,
+      dots: items[date][0]?.dots || [],
+    };
+    return termine;
+  }, {});
+
   return (
     <View style={{ flex: 1 }}>
       <Calendar
         markingType={'multi-dot'}
-
         markedDates={{
-          [selectedDate]: { selected: true, marked: true, selectedColor: 'blue' }, 
-          [items]: { dots:[undefineder] },
-          '2024-12-19': {dots:[undefineder]}
-
+          ...markedDates,
+          [selectedDate]: {
+            selected: true,
+            marked: true,
+            selectedColor: 'blue',
+          },
         }}
-        onDayPress={(day) => { setSelectedDate(day.dateString);
-        
-         }}
+        onDayPress={(day) => {
+          setSelectedDate(day.dateString);
+        }}
         firstDay={1}
         showWeekNumbers={false}
         enableSwipeMonths={true}
       />
-
     </View>
   );
-}
+};
 
 export default MyCalendar;
